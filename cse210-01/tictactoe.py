7534caf2bf98597561d75df5450aed4a9cf8aa6c
vallabisa <u068354659@byui.edu>
2022-09-18 09:56:38 +0800

    Update tictactoe.py

--- cse210-01/tictactoe.py
+++ cse210-01/tictactoe.py
@@ -8,6 +8,22 @@ def main():
     has_winner(board) == False
     draw(board) == False
 
+    while not (has_winner(board) == False and draw(board) == False):
+        print_board(board)
+        move(player, board)
+        player = next_player(player)
+    
+    print_board(board)
+    if has_winner(board) == True:
+        if player == 'x':
+            print("Player x wins!")
+        elif player == 'o':
+            print("Player o wins!")
+
+    elif has_winner(board) == False:
+        print("It's a draw.")
+    print("\nThank you for playing.")
+
 def print_board(board):
     print()
     print(f'{board[0]}|{board[1]}|{board[2]}')
@@ -23,6 +39,27 @@ def create_board():
     for square in range(9):
         board.append(square+1)
     return board
+
+def has_winner(board):
+    if board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[0] == board[3] == board[6] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
+        return True
+
+def draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True
+
+def next_player(current):
+    if current == "" or current == "o":
+        return "x"
+    elif current == "x":
+        return "o"
+
+def move(player, board):
+    square = int(input(f"{player}'s turn to choose a square (1-9): "))
+    board[square - 1] = player
+
 
 if __name__ == "__main__":
     main()
