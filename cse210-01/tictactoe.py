b60422b55470477e3970690414fa8010e9299a6c
vallabisa <u068354659@byui.edu>
2022-09-18 08:46:03 +0800

    has_winner

--- cse210-01/tictactoe.py
+++ cse210-01/tictactoe.py
@@ -25,3 +25,7 @@ def print_board(board):
     print('-+-+-')
     print(f'{board[6]}|{board[7]}|{board[8]}')
     print()
+
+def has_winner(board):
+    if board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[0] == board[3] == board[6] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
+        return True
