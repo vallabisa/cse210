b3bc7cc4fb9071de9421d35bfb5393922641e381
vallabisa <u068354659@byui.edu>
2022-09-18 08:46:27 +0800

    draw_board

--- cse210-01/tictactoe.py
+++ cse210-01/tictactoe.py
@@ -29,3 +29,9 @@ def print_board(board):
 def has_winner(board):
     if board[0] == board[1] == board[2] or board[3] == board[4] == board[5] or board[6] == board[7] == board[8] or board[0] == board[3] == board[6] or board[1] == board[4] == board[7] or board[2] == board[5] == board[8] or board[0] == board[4] == board[8] or board[2] == board[4] == board[6]:
         return True
+
+def draw(board):
+    for square in range(9):
+        if board[square] != "x" and board[square] != "o":
+            return False
+    return True
