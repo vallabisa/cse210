92a7bbd016e1bd69cc78df64ee8cd6d82e1cf3e7
vallabisa <u068354659@byui.edu>
2022-09-18 09:51:45 +0800

    Update tictactoe.py
    main function

--- cse210-01/tictactoe.py
+++ cse210-01/tictactoe.py
@@ -1,3 +1,14 @@
 #Assignment Week 1 Prove: Developer - Solo Code Submission
 # This program runs a tictactoe game
 #Author: Val Lorence Labisa
+
+def main():
+    player = next_player("")
+    board = create_board()
+    has_winner(board) == False
+    draw(board) == False
+
+
+
+if __name__ == "__main__":
+    main()
