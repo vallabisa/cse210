a012223cdd10c9a39cfc78b66e9ee63afb499a36
vallabisa <u068354659@byui.edu>
2022-09-18 08:42:30 +0800

    Created main function

--- cse210-01/tictactoe.py
+++ cse210-01/tictactoe.py
@@ -1,3 +1,10 @@
 #Assignment Week 1 Prove: Developer 
 #This program runs a tictactoe game
 #Author: Val Lorence Labisa
+
+def main():
+    player = next_player("")
+    board = create_board()
+    has_winner(board) == False
+    draw(board) == False
+
