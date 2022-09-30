ec19674f262a15b6d4ffe25be741e366f0ea305a
vallabisa <u068354659@byui.edu>
2022-09-30 13:32:24 +0800

    jumper based from seeker

---
+++ cse210-03/jumper-from -seeker/seeker/game/director.py
@@ -0,0 +1,66 @@
+from game.terminal_service import TerminalService
+from game.jumper import Hider
+from game.puzzle import Seeker
+
+
+class Director:
+    """A person who directs the game. 
+    
+    The responsibility of a Director is to control the sequence of play.
+
+    Attributes:
+        hider (Hider): The game's hider.
+        is_playing (boolean): Whether or not to keep playing.
+        seeker (Seeker): The game's seeker.
+        terminal_service: For getting and displaying information on the terminal.
+    """
+
+    def __init__(self):
+        """Constructs a new Director.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        self._hider = Hider()
+        self._is_playing = True
+        self._seeker = Seeker()
+        self._terminal_service = TerminalService()
+        
+    def start_game(self):
+        """Starts the game by running the main game loop.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self._is_playing:
+            self._get_inputs()
+            self._do_updates()
+            self._do_outputs()
+
+    def _get_inputs(self):
+        """Moves the seeker to a new location.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        guess_letter = self._terminal_service.read_text("\nGuess a letter [a-z]: ")
+        self._seeker.move_location(new_location)
+        
+    def _do_updates(self):
+        """Keeps watch on where the seeker is moving.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        self._hider.watch_seeker(self._seeker)
+        
+    def _do_outputs(self):
+        """Provides a hint for the seeker to use.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        hint = self._hider.get_hint()
+        self._terminal_service.write_text(hint)
+        if self._hider.is_found():
+            self._is_playing = False
