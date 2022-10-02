43de97ca6de546c0df9d450124f6a82f3d875f9f
vallabisa <u068354659@byui.edu>
2022-10-02 11:34:12 +0800

    update

--- cse210-03/Jumper/game/secret.py
+++ cse210-03/Jumper/game/secret.py
@@ -1,12 +1,12 @@
 import random
 class Secret:
     """
-    Reads from a file and returns a random word 
+    Selects a random word 
     """
     def __init__(self):
         self._words = ""
     
     def get_word(self):
-        self._words = "answer"
+        self._words = "answer jumper puzzle"
         self._words = list(map(str, self._words.split()))
         return random.choice(self._words)
